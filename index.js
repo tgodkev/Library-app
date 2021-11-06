b664677a5ee1fa8068a90c79f7f3bec613c4e335
kevin knight <tgodkev@gmail.com>
2021-11-06 01:37:59 -0400

    forms and button done, css fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let myLibrary = [];
 
 
 
-// constructor function for book
+// constructor function for book.
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,11 +11,16 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.book = function() {
-        return `${this.title} by ${this.author}`;
+        return `${this.title} by ${this.author} ${this.read}`;
     }
 };
 
+
+//calling the constructor function.
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
 const lotr = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 400, true);
-theHobbit.book();
-lotr.book();
+
+console.log(theHobbit.book());
+console.log(lotr.book());
+
+
