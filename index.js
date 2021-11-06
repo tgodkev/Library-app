c6a289405e9d255c3a10445fc9b8043caf3f5de8
kevin knight <tgodkev@gmail.com>
2021-11-05 23:05:19 -0400

    some basic styling

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 let myLibrary = [];
+const pages = document.getElementById('pages');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
 
-const addBook = document.querySelector('#addBook');
 
 
 
-function Book(name, author, pages, read) {
-    this.name = name;
+
+
+function addedBook() {
+  let book = new Book(title.value, author.value, pages.value, read.value);
+  myLibrary.push(book);
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.thing = function() {
-      console.log(name, author, pages, read,);
-    }
 }
-
-const book1 = new Book("lord of the rings,", "jr tolkein, ", "1 million,", "has read");
-book1.thing();
-
-function addBookToLibrary() {
-
-
-  // do stuff here
-}
-
-
 
 
 
 
 $('#exampleModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
+  var addedBook = button.data('whatever') 
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + addedBook)
+  modal.find('.modal-body input').val(addedBook)
+  
+  
+  
+  
+  
+  // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
   modal.find('.modal-title').text('New message to ' + recipient)
   modal.find('.modal-body input').val(recipient)
-})
+})};
