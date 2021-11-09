1fc8a49c865ef121471fc257250aa10e39cc8e94
kevin knight <tgodkev@gmail.com>
2021-11-08 22:36:01 -0500

    completed with referincing another submission.

--- index.js
+++ index.js
@@ -1,55 +1,137 @@
-// variables and buttons.
+// DOM elements for buttons
+const addBtn = document.querySelector('#newBook');
+const addForm = document.querySelector('#addForm');
+const submitBtn = document.querySelector('#submit');
+
+// Array
 let myLibrary = [];
-const addBook = document.getElementById('addBook');
-addBook.addEventListener('click', userBook);
 
-
-// constructor function for book.
-
+// Constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.book = function() {
-        return `${this.title} by ${this.author} ${this.read}`;
+    this.button = document.createElement('button');
+    this.clear = document.createElement('button');
+}
+// method attached to each book to change read status
+Book.prototype.readStatus = function() {
+    if (this.read == "read") {
+        this.read = "unread";
+    } else {
+        this.read = "read";
     }
+    clearTable();
+    displayBooks();
 };
 
 
-//calling the constructor function.
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-const lotr = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 400, true);
 
-console.log(theHobbit.book());
-console.log(lotr.book());
+// Function to pre-load some books into Library Array
+function addBooksToLibrary(obj) {
+    myLibrary.push(obj);
+}
+//adding books to library pre user
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", '1,225', "read");
+const theHobbit = new Book("The Hobbit", "JRR Tolkien", "298", "unread");
+const aSpell = new Book ("A Spell for Chameleon", "Piers anthony", "344", "read")
+addBooksToLibrary(warAndPeace);
+addBooksToLibrary(theHobbit);
+addBooksToLibrary(aSpell);
 
-// function to add books to library.
-function userBook() {
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let read = document.getElementById('read').value;
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    createNewCard();
-};
+// function for users to add books to array via form input
+function addBookToLibrary() {
+    const title = document.querySelector('#title-input').value;
+    const author = document.querySelector('#author-input').value;
+    const pages = document.querySelector('#pages-input').value;
+    let read = document.querySelector('#read-input').checked;
+    if (read == true) {
+        read = "read";
+    } else {
+        read = "unread";
+    } ;
+    addForm.reset();
+    addForm.hidden = true;
+    addBtn.hidden = false;
+    submitBtn.hidden = true;
+    myLibrary.push(new Book(title, author, pages, read));
+    clearTable();
+    displayBooks();
+}
 
 
-//trying to create a new card that dsiplays the book info.
-function createNewCard() {
-    let card = document.createElement('div');
-    card.className = 'card';
-    let cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-    let cardTitle = document.createElement('h5');
-    cardTitle.className = 'card-title';
-    let cardAuthor = document.createElement('p');
-    cardAuthor.className = 'card-author';
-    let cardPages = document.createElement('p');
-    cardPages.className = 'card-pages';
-    let cardRead = document.createElement('p');
-    cardRead.className = 'card-read';
+
+// Clear table func
+let clearTable = function() {
+    const table = document.getElementById('table');
+    table.innerHTML = '';
+} 
+
+
+let myTable = document.querySelector('#table');
+let headers = ['Title', 'Author', 'Pages', 'Read/Unread', 'Update Read Status', 'Remove From Library'];
+
+// function that displays the current library into a table and 
+// makes applicable buttons for read and remove per object. 
+function displayBooks() {
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
     
-};
+    table.appendChild(headerRow);
+
+    myLibrary.forEach(book => {
+        let row = document.createElement('tr');         
+        Object.values(book).forEach(text =>{
+            if(text == book.button) {
+                let cell = document.createElement('td');
+                let makeButton = document.createElement('button');
+                makeButton.innerHTML = "Update";
+                makeButton.addEventListener("click", () => book.readStatus());
+                cell.appendChild(makeButton);
+                row.appendChild(cell);
+            } else if (text == book.clear) {
+                let cell = document.createElement('td');
+                let makeButton = document.createElement('button');
+                makeButton.innerHTML = "Remove";
+                makeButton.addEventListener("click", () => {
+                    for(var i = 0; i < myLibrary.length; i++) {
+                        if (myLibrary[i].title == book.title) {
+                            myLibrary.splice([i], 1);
+                        
+                        }
+                    } 
+                    clearTable(); 
+                    displayBooks();});
+                cell.appendChild(makeButton);
+                row.appendChild(cell);
+            } else {
+            let cell = document.createElement('td');
+            let textNode = document.createTextNode(text);
+            cell.appendChild(textNode);
+            row.appendChild(cell);
+            }
+        });
+        table.appendChild(row);
+    });
+
+    myTable.appendChild(table);
+}
+
+displayBooks();
+
+
+// Event listeners
+addBtn.addEventListener('click', () => {
+    addForm.hidden = false; 
+    addBtn.hidden = true;
+    submitBtn.hidden = false;
+})
+submitBtn.addEventListener('click', addBookToLibrary);
